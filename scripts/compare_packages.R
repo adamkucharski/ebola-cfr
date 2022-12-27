50506b95868ecba76e0b5c9cfe69855f2af9e023
adamkucharski <adam.kucharski@lshtm.ac.uk>
2022-12-27 20:59:11 +0000

    Add comparisons with stan models

---
+++ scripts/compare_packages.R
@@ -0,0 +1,85 @@
+# - - - - - - - - - - - - - - - - - - - - - - - 
+# Comparison of EpiLine and Dynamical Truncation
+# Examples from below repos, reproduced here for illustrative comparison:
+# https://github.com/parksw3/dynamicaltruncation/
+# https://github.com/BDI-pathogens/EpiLine
+# - - - - - - - - - - - - - - - - - - - - - - - 
+
+# Load libraries
+library(tidyverse)
+library(devtools)
+library(data.table)
+library(purrr)
+
+install_github("BDI-pathogens/EpiLine")
+library(EpiLine)
+
+install_github("parksw3/dynamicaltruncation")
+library(dynamicaltruncation)
+
+# EpiLine simulation ------------------------------------------------------
+
+set.seed( 1 )
+
+# define the length of the simulatiopn
+t_rep          <- 50 # length of time for which data is reported
+t_symptom_pre  <- 30 # time before the reporting period to simulate
+t_symptom_post <- 5  # time after the reporting period to simulate
+t_max          <- t_rep + t_symptom_post + t_symptom_pre
+
+# set up the variable r(t) and distribution
+symptom_0 <- 2                                # initial number of symptomatic people
+r         <- 0.1 - 0.13 * ( 1:t_max ) / t_max # r(t) in the simulation
+xi        <- -1 + 6 * ( t_max:1 ) / t_max          # xi parameter in the symptom-report dist
+lambda    <- 2 + ( t_max:1 ) / t_max         # lambda parameter in the symptom-report dist
+
+simulation <- symptom_report.simulator(
+  t_rep          = t_rep,
+  t_symptom_pre  = t_symptom_pre,
+  t_symptom_post = t_symptom_post,
+  symptom_0   = symptom_0,
+  r           = r,
+  dist_xi     = xi,
+  dist_lambda = lambda
+)
+
+# Fit model:
+
+# data 
+reported    <- simulation$reported
+ll_report   <- simulation$linelist$report
+ll_symptom  <- simulation$linelist$symptom
+report_date <- as.Date("2022-04-01")
+
+# fit using model
+mcmc_n_samples <- 100
+mcmc_n_chains  <- 1
+fit <- symptom_report.fit( reported, ll_symptom, ll_report, report_date = report_date, 
+                           mcmc_n_samples = mcmc_n_samples, mcmc_n_chains = mcmc_n_chains )
+
+fit$plot.symptoms( simulation = simulation)
+
+# Estimation with dynamical truncation ------------------------------------
+
+outbreak <- simulate_gillespie(seed = 101)
+secondary_dist <- data.table( meanlog = 1.8, sdlog = 0.5) |> add_natural_scale_mean_sd()
+
+obs <- outbreak |>
+  simulate_secondary(
+    meanlog = secondary_dist$meanlog[[1]],
+    sdlog = secondary_dist$sdlog[[1]]
+  ) |> observe_process()
+
+truncated_obs <- obs |>
+  filter_obs_by_obs_time(obs_time = 25) |>
+  DT(sample(1:.N, 200, replace = FALSE))
+
+truncated_cases <- construct_cases_by_obs_window(
+  obs, windows = c(25), obs_type = "stime"
+)
+
+plot_cases_by_obs_window(truncated_cases)
+
+naive_fit <- naive_delay(data = truncated_obs, cores = 4, refresh = 0)
+
+
--- scripts/script_2022.R
+++ scripts/script_2022.R
@@ -10,6 +10,9 @@ library(devtools)
 library(incidence2)
 install_github("epiverse-trace/epiparameter")
 library(epiparameter)
+library(fitdistrplus)
+
+# setwd("~/Documents/GitHub/ebola-cfr/")
 
 # Define probability mass function for onset-to-death
 onset_to_death_ebola <- epiparameter::epidist("ebola","onset_to_death")
@@ -30,9 +33,6 @@ data_ebola <- read_csv("https://3mmuwilir3.execute-api.eu-central-1.amazonaws.co
 data_ebola <- data_ebola |> mutate(Date_case = as.Date(ifelse(!is.na(Date_onset),Date_onset,Date_confirmation),"1970-01-01"))
 data_outcome <- data_ebola %>% filter(!is.na(Date_Death) | !is.na(Date_Recovered))
 
-# Output estimate based on known outcomes
-CFR_binomial <- prop.test(x=sum(!is.na(data_outcome$Date_Death)), n=nrow(data_outcome))
-c(CFR_binomial$estimate,CFR_binomial$conf.int) |> c.text()
 
 # Estimate CFR from incidence data --------------------------------------------------
 case_data <- incidence2::incidence(data_ebola,Date_case) |> complete_counts()
@@ -40,13 +40,18 @@ death_data <- incidence2::incidence(data_ebola,Date_Death) |> complete_counts()
 
 # Plot data:
 plot(case_data$date_index,case_data$count,ylim=c(0,1.2*max(case_data$count)),
-     type="s",lwd=2,col="black",ylab="cases and deaths",xlab="",yaxs="i",main=labelx) # Plot naive CFR
+     type="s",lwd=2,col="black",ylab="cases and deaths",xlab="",yaxs="i",main="") # Plot naive CFR
 lines(death_data$date_index,death_data$count,type="s",lwd=2,col="orange") # Plot naive CFR
 
 # Estimate CFR:
 out_cfr <- scale_cfr(case_incidence = case_data$count,
                      death_incidence = death_data$count,
                      delay_fun = onset_to_death_ebola)
+
+
+# Output estimate based on known outcomes
+CFR_binomial <- prop.test(x=sum(!is.na(data_outcome$Date_Death)), n=nrow(data_outcome))
+c(CFR_binomial$estimate,CFR_binomial$conf.int) |> c.text()
 
 # Output unadjusted ('naive') CFR and 95% CI:
 c.text(out_cfr$nCFR)
@@ -55,3 +60,38 @@ c.text(out_cfr$nCFR)
 c.text(out_cfr$cCFR)
 
 
+# Add comparison with EpiLine --------------------------------------------------
+
+install_github("BDI-pathogens/EpiLine")
+library(EpiLine)
+
+# Filter on known onset and deaths
+data_death <- data_ebola %>% filter(!is.na(Date_onset) & !is.na(Date_Death)) %>% arrange(Date_onset)
+
+# Simple fitdistrplus fitting:
+day_differences <- as.numeric(data_death$Date_Death-data_death$Date_onset)
+param_index <- fitdistrplus::fitdist(day_differences,"lnorm") 
+
+xx <- 1:40
+hist(day_differences,prob=T,xlab="days",main="onset-to-death")
+lines(xx,dlnorm(xx,param_index$estimate[["meanlog"]],param_index$estimate[["sdlog"]]))
+
+
+# Define vectors
+report_date <- min(data_outcome$Date_case,na.rm=T)
+reported    <- case_data$count 
+ll_symptom  <- as.numeric(data_death$Date_onset - report_date)
+ll_outcome   <- as.numeric(data_death$Date_Death - report_date)
+
+# fit using EpiLine model
+mcmc_n_samples <- 100
+mcmc_n_chains  <- 1
+fit <- symptom_report.fit( reported, ll_symptom, ll_outcome, report_date = report_date, 
+                           mcmc_n_samples = mcmc_n_samples, mcmc_n_chains = mcmc_n_chains )
+
+# Plot posterior time series estimates
+fit$plot.symptoms()
+
+# Plot posterior parameters estimate
+fit$plot.r() # Growth rate
+fit$plot.symptom_report.dist() # Onset-to-death interval
